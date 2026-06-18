d4d717e9ec1e1bf8754ffdf1147c0ebc5e50a9be
Ali Moghimi <mghmiali@gmail.com>
2026-06-18 18:29:40 +0330

    fix: constraint for balance/credit GE reserved credit

--- src/infra/db/models/balance.py
+++ src/infra/db/models/balance.py
@@ -32,6 +32,7 @@ class Balance(Base):
 
     __table_args__ = (
         CheckConstraint("credits >= 0", name="check_credits_non_negative"),
+        CheckConstraint("credits >= reserved_credits", name="check_credits_ge_reserved_credits"),
         CheckConstraint("reserved_credits >= 0", name="check_reserved_credits_non_negative"),
     )
 
